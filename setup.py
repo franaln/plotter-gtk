22879daff1d1296a9b4971ac743be53aa3ba48c9
franaln <franaln@gmai.com>
2016-03-12 19:22:22 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='plotter',
+    version='0.1dev',
+    description='',
+    url='https://github.com/franaln/plotter-gtk',
+    author='Francisco Alonso',
+    author_email='franaln@gmail.com',
+    license='',
+    packages=['plotter',],
+    scripts=['bin/plotter',],
+    zip_safe=False,
+)
